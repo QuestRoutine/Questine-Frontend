fb7c7bf52d1e892d99c93e3904ba21cfe669e299
Elbyss <elysiabyss@gmail.com>
2025-04-28 18:23:23 +0900

    style: change bottom bar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Award, House, UserCog } from 'lucide-react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -10,10 +11,7 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        // tabBarButton: HapticTab,
-        // tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
@@ -26,8 +24,26 @@ export default function TabLayout() {
       <Tabs.Screen
         name='index'
         options={{
-          title: 'Home',
-          // tabBarIcon: ({ color }) => <IconSymbol size={28} name='house.fill' color={color} />,
+          title: '홈',
+          tabBarActiveTintColor: 'hotpink',
+          tabBarIcon: ({ color }) => <House size={20} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name='Achievement'
+        options={{
+          title: '업적',
+          tabBarActiveTintColor: 'hotpink',
+          tabBarIcon: ({ color }) => <Award size={20} color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+        name='Profile'
+        options={{
+          title: '프로필',
+          tabBarActiveTintColor: 'hotpink',
+          tabBarIcon: ({ color }) => <UserCog size={20} color={color} />,
         }}
       />
     </Tabs>
