d2dc7ecc2cbbc9e43b926e6bd2e5100f56020b1e
Elbyss <elysiabyss@gmail.com>
2025-05-22 03:11:56 +0900

    Chore: change ip addr

--- api/axios.ts
+++ api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 
 const axiosInstance = axios.create({
-  baseURL: Platform.OS === 'ios' ? 'http://localhost:4000' : 'http://10.0.2.2:4000',
+  baseURL: Platform.OS === 'ios' ? 'http://172.30.1.49:4000' : 'http://10.0.2.2:4000',
 });
 
 // axiosInstance.interceptors.request.use(
