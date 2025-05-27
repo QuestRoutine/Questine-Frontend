ea4a48d483d240bf73da62a45f190ca820fa281f
Elcynia <elysiabyss@gmail.com>
2025-05-27 16:42:29 +0900

    Feat: add axios interceptor

--- api/axios.ts
+++ api/axios.ts
@@ -1,3 +1,4 @@
+import { getSecureStore } from '@/utils/secureStore';
 import axios from 'axios';
 import { Platform } from 'react-native';
 
@@ -5,26 +6,12 @@ const axiosInstance = axios.create({
   baseURL: Platform.OS === 'ios' ? 'http://172.30.1.49:4000' : 'http://10.0.2.2:4000',
 });
 
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     // Add authorization token to headers if available
-//     const token = localStorage.getItem('accessToken');
-//     if (token) {
-//       config.headers['Authorization'] = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.request.use(async (config) => {
+  const accessToken = await getSecureStore('accessToken');
+  if (accessToken) {
+    if (config.headers) config.headers['Authorization'] = `Bearer ${accessToken}`;
+  }
+  return config;
+});
 
-// axiosInstance.interceptors.response.use(
-//   (response) => {
-//     return response.data;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
 export default axiosInstance;
