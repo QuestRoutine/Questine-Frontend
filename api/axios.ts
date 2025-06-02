2cf69400bb902fda6613d14dee85ddb09943d571
elcynia <elysiabyss@gmail.com>
2025-06-02 21:40:21 +0900

    Chore: add environment

--- api/axios.ts
+++ api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 
 const axiosInstance = axios.create({
-  baseURL: Platform.OS === 'ios' ? 'http://172.30.1.49:4000' : 'http://10.0.2.2:4000',
+  baseURL: Platform.OS === 'ios' ? `${process.env.EXPO_PUBLIC_IOS_URL}` : `${process.env.EXPO_PUBLIC_ANDROID_URL}`,
 });
 
 axiosInstance.interceptors.request.use(async (config) => {
